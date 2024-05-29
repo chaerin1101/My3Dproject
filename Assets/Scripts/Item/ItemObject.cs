53f1f3f2d05f8f21664ed2da96088314ee0074ed
chaerin1101 <167056216+chaerin1101@users.noreply.github.com>
2024-05-29 20:57:22 +0900

    [feat]Item

---
+++ Assets/Scripts/Item/ItemObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    public ItemData data;
+    // 상호작용 할때 필요한 기능을 넣어줄 것입니다.
+}
