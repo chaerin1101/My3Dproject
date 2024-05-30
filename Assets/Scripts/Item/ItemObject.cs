0396b18dd6650b11625823936ea08e20c5184ed5
chaerin1101 <167056216+chaerin1101@users.noreply.github.com>
2024-05-30 14:38:16 +0900

    [feat]Aim

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -2,8 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemObject : MonoBehaviour
+public interface IInteractable
 {
-    public ItemData data;
-    // 상호작용 할때 필요한 기능을 넣어줄 것입니다.
+    public string GetInteractPrompt(); // 화면에 띄워줄 프롬프트에 관련된 함수
+    public void OnInteract(); // 인터렉트 됐을 때 어떤 효과를 발생시키게 할건지
+}
+
+public class ItemObject : MonoBehaviour, IInteractable
+{
+    public ItemData data; // 상호작용 할때 필요한 기능을 넣어줄 것입니다.
+
+    public string GetInteractPrompt()
+    {
+        string str = $"{data.displayName}\n{data.description}";
+        return str;
+    }
+
+    public void OnInteract()
+    {
+        CharacterManager.Instance.Player.itemData = data;
+        CharacterManager.Instance.Player.addItem?.Invoke();
+        Destroy(gameObject);
+    }
 }
